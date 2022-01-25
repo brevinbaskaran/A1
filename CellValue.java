46fdff909b9b01bdeb5b8d743fdf6bc1c5f4b8aa
Saurav Guduru <sauravg2803@gmail.com>
2022-01-24 21:48:44 -0500

    updated placement of code

--- CellValue.java
+++ CellValue.java
@@ -8,11 +8,8 @@
 // compare this file with GameState and add the following cell values 	EMPTY, X, O
 
 public enum CellValue {
-
+// your code here
     EMPTY, // cell is empty
     X,      // cell is filled with X
     O       // cell is filled with O
-
-// your code here
-// put the code here
 }
